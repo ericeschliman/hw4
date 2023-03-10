8ac6682736ba1645ffd5d4a8779ed0b2778d9c89
ericeschliman <eric.eschliman@kellogg.northwestern.edu>
2023-03-10 13:35:22 +0000

    finished file attachment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,13 +10,17 @@ class PostsController < ApplicationController
   end
 
   def create
+    if @current_user
     @post = Post.new
     @post["title"] = params["post"]["title"]
+    @post.uploaded_image.attach(params["post"]["uploaded_image"])
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
     @post["place_id"] = params["post"]["place_id"]
     @post.save
+    else
+      flash["notice"] = "Login first."
+    end
     redirect_to "/places/#{@post["place_id"]}"
   end
-
 end
